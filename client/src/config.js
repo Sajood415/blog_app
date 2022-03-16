344ce870b1f7864983e0945fb3252110387029b5
Sajood Ur Rehman <62991779+Sajood415@users.noreply.github.com>
2022-03-16 10:06:38 +0500

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,12 +1,12 @@
 const oktaAuthConfig = {
-    issuer: 'https://dev-70170495.okta.com/oauth2/default',
-    clientId: '0oa462t9zac4mGAzZ5d7',
+    issuer: '',
+    clientId: '',
     redirectUri: window.location.origin + '/login/callback',
 };
   
 const oktaSignInConfig = {
-    baseUrl: 'https://dev-70170495.okta.com',
-    clientId: '0oa462t9zac4mGAzZ5d7',
+    baseUrl: '',
+    clientId: '',
     redirectUri: window.location.origin + '/login/callback',
     logo: 'blog.png',
     authParams: {
